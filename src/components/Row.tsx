2fd8055e326700c52cb8d5452b6a2962c5ecc44b
veserion <lobanovdanil97@gmail.com>
2025-01-11 17:53:38 +0300

    puff screen wip

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,6 +1,15 @@
 import styled from "@emotion/styled";
 
+type TProps = {
+  gap?: string;
+  width?: string;
+  alignItems?: string;
+};
+
 export const Row = styled.div`
   display: flex;
   justify-content: space-between;
+  gap: ${(props: TProps) => props.gap};
+  width: ${(props: TProps) => props.width};
+  align-items: ${(props: TProps) => props.alignItems};
 `;
--- src/screens/PuffScreen/PuffScreen.tsx
+++ src/screens/PuffScreen/PuffScreen.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import { Headline } from "../../components/Text";
+
+import { Body, Headline } from "@components/Text.tsx";
+import { Row } from "@components/Row.tsx";
+import { StatTitle, StatValue } from "@screens/ProfileScreen/styles.ts";
+import { Column } from "@components/Column.tsx";
+import { Button } from "@components/Button.tsx";
+import { SecondaryButton } from "@components/SecondaryButton.tsx";
 
 const Root = styled.div`
   display: flex;
@@ -8,11 +14,43 @@ const Root = styled.div`
   gap: 24px;
 `;
 
+export const ProfileCard = styled.div`
+  width: 50%;
+  background: linear-gradient(240deg, rgba(54, 161, 15, 0.2) 0%, rgba(54, 161, 15, 0.12) 100%);
+  border-radius: 12px;
+  padding: 12px;
+  height: 72px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
 export const PuffScreen: React.FC = () => {
   return (
     <Root>
-      <Headline>Proof of Puff</Headline>
-      {/* TODO: Add Proof of Puff content */}
+      <Column gap="16px">
+        <Headline>Smoke DAO bot</Headline>
+        <Row gap="8px">
+          <ProfileCard>
+            <StatTitle>Smoke ID</StatTitle>
+            <StatValue>9 851 880</StatValue>
+          </ProfileCard>
+          <ProfileCard>
+            <StatTitle>Total $moken</StatTitle>
+            <StatValue>14 880</StatValue>
+          </ProfileCard>
+        </Row>
+        <Button fullwidth={true} onClick={() => {}}>
+          Start Smoke Session
+        </Button>
+        <Body>
+          Start a smokey session every time you smoke with friends. Scan the received QR code and
+          get $moken.
+        </Body>
+        <SecondaryButton fullwidth={true} onClick={() => {}}>
+          High Sosciety chat
+        </SecondaryButton>
+      </Column>
     </Root>
   );
-}; 
+};
