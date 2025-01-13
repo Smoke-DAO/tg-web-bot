4df09af5c4f1063f2512128a39f6130aaa8861c8
veserion <lobanovdanil97@gmail.com>
2025-01-13 23:36:19 +0300

    puff screen was done

--- src/screens/PuffScreen/PuffScreen.tsx
+++ src/screens/PuffScreen/PuffScreen.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import styled from "@emotion/styled";
 
-import { Body, Headline } from "@components/Text.tsx";
-import { Row } from "@components/Row.tsx";
-import { StatTitle, StatValue } from "@screens/ProfileScreen/styles.ts";
-import { Column } from "@components/Column.tsx";
 import { Button } from "@components/Button.tsx";
+import { Column } from "@components/Column.tsx";
+import { Row } from "@components/Row.tsx";
 import { SecondaryButton } from "@components/SecondaryButton.tsx";
+import { Body, Headline } from "@components/Text.tsx";
+import { StatTitle, StatValue } from "@screens/ProfileScreen/styles.ts";
+import { useState } from "react";
+import { SmokeSession } from "@screens/PuffScreen/SmokeSession.tsx";
 
 const Root = styled.div`
   display: flex;
@@ -26,31 +28,37 @@ export const ProfileCard = styled.div`
 `;
 
 export const PuffScreen: React.FC = () => {
+  const [openSmokeSession, setOpenSmokeSession] = useState(false);
+  const smokeId = "9851880";
+
   return (
     <Root>
-      <Column gap="16px">
-        <Headline>Smoke DAO bot</Headline>
-        <Row gap="8px">
-          <ProfileCard>
-            <StatTitle>Smoke ID</StatTitle>
-            <StatValue>9 851 880</StatValue>
-          </ProfileCard>
-          <ProfileCard>
-            <StatTitle>Total $moken</StatTitle>
-            <StatValue>14 880</StatValue>
-          </ProfileCard>
-        </Row>
-        <Button fullwidth={true} onClick={() => {}}>
-          Start Smoke Session
-        </Button>
-        <Body>
-          Start a smokey session every time you smoke with friends. Scan the received QR code and
-          get $moken.
-        </Body>
-        <SecondaryButton fullwidth={true} onClick={() => {}}>
-          High Sosciety chat
-        </SecondaryButton>
-      </Column>
+      {!openSmokeSession && (
+        <Column gap="24px">
+          <Headline>Smoke DAO bot</Headline>
+          <Row gap="8px">
+            <ProfileCard>
+              <StatTitle>Smoke ID</StatTitle>
+              <StatValue>{smokeId}</StatValue>
+            </ProfileCard>
+            <ProfileCard>
+              <StatTitle>Total $moken</StatTitle>
+              <StatValue>14 880</StatValue>
+            </ProfileCard>
+          </Row>
+          <Button fullwidth={true} onClick={() => setOpenSmokeSession(true)}>
+            Start Smoke Session
+          </Button>
+          <Body>
+            Start a smokey session every time you smoke with friends. Scan the received QR code and
+            get $moken.
+          </Body>
+          <SecondaryButton fullwidth={true} onClick={() => {}}>
+            High Society chat
+          </SecondaryButton>
+        </Column>
+      )}
+      {openSmokeSession && <SmokeSession smokeId={smokeId} />}
     </Root>
   );
 };
